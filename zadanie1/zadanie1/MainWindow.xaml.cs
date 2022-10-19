3670fb48f1665c7beda6d96d9fa710b02fda893c
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2022-10-19 16:30:49 +0200

    zad1: add searching nearby devices

--- zadanie1/zadanie1/MainWindow.xaml.cs
+++ zadanie1/zadanie1/MainWindow.xaml.cs
@@ -15,13 +15,17 @@ using System.Windows.Shapes;
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
 using InTheHand.Net;
+using System.Windows.Forms;
+
 
 namespace zadanie1
 {
     public partial class MainWindow : Window
     {
         BluetoothDeviceInfo[] btDevices;
+        BluetoothDeviceInfo pairedDevice = null;
         BluetoothClient btClient = new BluetoothClient();
+        String selectedDevice = null;
 
         public MainWindow()
         {
@@ -30,19 +34,83 @@ namespace zadanie1
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            await Task.Run(() => btDevices = btClient.DiscoverDevices().ToArray());
+            scanBtn.IsEnabled = false;
+            scanBtn.Content = "Skanowanie...";
+            await Task.Run(() =>
+            {
+                btDevices = btClient.DiscoverDevices().ToArray();
+            }
+            );
             devicesComboBox.Items.Clear();
             foreach (BluetoothDeviceInfo device in btDevices)
             {
                 devicesComboBox.Items.Add(device.DeviceName);
             }
-            
-            
+            scanBtn.IsEnabled = true;
+            scanBtn.Content = "Skanuj";
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            selectedDevice = devicesComboBox.SelectedItem.ToString();
+            selectedDeviceLabel.Content = (selectedDevice);
+        }
 
+        private void Pair_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedDevice == null) return;
+
+            foreach (BluetoothDeviceInfo device in btDevices)
+            {
+                if (device.DeviceName.Equals(selectedDevice)) {
+                    pairedDevice = device;
+
+                    pairedDevice.Refresh();
+                    pairedDevice.SetServiceState(BluetoothService.ObexObjectPush, true);
+                    break;
+                }
+            }
+
+            if (pairedDevice == null)
+            {
+                pairDeviceBtn.Content = "Null";
+                return;
+            }
+
+
+            if (!pairedDevice.Authenticated && BluetoothSecurity.PairRequest(pairedDevice.DeviceAddress, "12345"))
+            {
+                pairDeviceBtn.Content = "Połączono";
+            }
+            else
+            {
+                pairDeviceBtn.Content = "Nie udało się połączyć";
+            }
+        }
+        
+       
+        private void Choose_File_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filenameLabel.Content = openFileDialog.FileName;
+            }
+        }
+
+        private void Send_File_Click(object sender, RoutedEventArgs e)
+        {
+            String filename = filenameLabel.Content.ToString();
+            if (filename == "" || filename == null) return;
+
+            var uri = new Uri("obex://" +  pairedDevice.DeviceAddress + "/" + filename);
+            var request = new ObexWebRequest(uri);
+            request.ReadFile(filename);
+            var response = (ObexWebResponse) request.GetResponse();
+            // check response.StatusCode
+            response.Close();
         }
     }
 }
